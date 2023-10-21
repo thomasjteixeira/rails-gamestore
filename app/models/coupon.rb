9b5d48dae6985f478b611366318c131a748b8ae7
Thómas Jéfferson <thomasjteixeira@gmail.com>
2023-10-21 16:09:51 -0300

    feat: Coupon model and tests

---
+++ app/models/coupon.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class Coupon < ApplicationRecord
+  validates :code, presence: true, uniqueness: { case_sensitive: false }
+  validates :status, :due_date, presence: true
+
+  validates :discount_value, presence: true, numericality: { greater_than: 0 }
+
+  enum status: { active: 1, inactive: 2 }
+end
