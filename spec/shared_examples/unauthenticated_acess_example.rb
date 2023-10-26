7f9dc24849a6c0ee3431eddfb29f6ded197650cc
Thómas Jéfferson <thomasjteixeira@gmail.com>
2023-10-26 12:00:02 -0300

    feat: unauthenticated requests tests in admin routes

---
+++ spec/shared_examples/unauthenticated_acess_example.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+shared_examples 'unauthenticated access' do
+  it 'return unauthorized status' do
+    expect(response).to have_http_status(:unauthorized)
+  end
+end
